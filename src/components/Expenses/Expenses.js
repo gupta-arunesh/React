be7b86668256d2180d2d01732068a12d3399058a
Arunesh kumar <arunesh@gmail.com>
2021-06-03 21:23:07 +0530

    5section -> complete section ( BUG -- not fixed )

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -24,6 +25,7 @@ const Expenses = (props) => {
           onChangeFilter={filterChangeHandler}
         />
 
+        <ExpensesChart Expenses={filteredExpesnse} />
         <ExpenseList items={filteredExpesnse} />
       </Card>
     </div>
